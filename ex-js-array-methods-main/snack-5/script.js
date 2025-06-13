eaab90e0b31bffb6184468982bb3cd9d92fa0019
marzia <you@example.com>
2025-06-13 16:15:58 +0200

    implement logic for JS ex. n. 5

--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -2,3 +2,22 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+const pari = [];
+
+for (let i = 0; i < nums.length; i++) {
+  const num = nums[i];
+  if (num % 2 === 0) {
+    console.log(num);
+  }
+}
+
+nums.forEach((num, i, array) => {
+  if (num % 2 === 0) {
+    console.log(num);
+  }
+});
+
+const parissimi = nums.forEach((numb) => numb % 2 === 0);
+
+console.log(parissimi);
