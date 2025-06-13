f60669edd7eb686399d33a8c52c3698058cb4358
marzia <you@example.com>
2025-06-13 18:12:46 +0200

    implement logic of js ex n. 8

--- ex-js-array-methods-main/snack-8/script.js
+++ ex-js-array-methods-main/snack-8/script.js
@@ -1,23 +1,30 @@
 const students = [
   {
     id: 1,
-    name: 'Marco Lanci',
+    name: "Marco Lanci",
     age: 32,
-    class: '3C'
+    class: "3C",
   },
   {
     id: 2,
-    name: 'Mario Banfi',
+    name: "Mario Banfi",
     age: 34,
-    class: '4A'
+    class: "4A",
   },
   {
     id: 3,
-    name: 'Luigi Banzi',
+    name: "Luigi Banzi",
     age: 33,
-    class: '5B'
+    class: "5B",
   },
 ];
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+for (let i = 0; i < students.length; i++) {
+  const student = students[i];
+  if (student.name === "Marco Lanci") {
+    console.log(student.class);
+  }
+}
