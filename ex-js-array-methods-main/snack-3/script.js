3950e797267536cc2c7d5b7c2e0bdf629f02e290
marzia <you@example.com>
2025-06-13 15:54:20 +0200

    implement logic for JS ex. n. 3

--- ex-js-array-methods-main/snack-3/script.js
+++ ex-js-array-methods-main/snack-3/script.js
@@ -2,3 +2,9 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+numbers.map((number, i, array) => {
+  const plus = number + 1;
+  console.log(plus);
+  return plus;
+});
