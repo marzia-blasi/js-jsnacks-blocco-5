a5a2df08861120d99609749366317a783cb88b17
marzia <you@example.com>
2025-06-13 15:31:01 +0200

    Implemented logic for JS exercises 1 and 2

--- ex-js-array-methods-main/snack-2/script.js
+++ ex-js-array-methods-main/snack-2/script.js
@@ -1,8 +1,19 @@
 const people = [
-  { name: 'Paolo', age: 35 },
-  { name: 'Giulia', age: 24 },
-  { name: 'Marco', age: 67 }
+  { name: "Paolo", age: 35 },
+  { name: "Giulia", age: 24 },
+  { name: "Marco", age: 67 },
 ];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+for (let i = 0; i < people.length; i++) {
+  const person = people[i];
+  console.log(person.name);
+}
+
+let persone;
+
+people.forEach((person, i, people) => {
+  console.log(person.name);
+});
